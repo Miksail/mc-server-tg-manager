129b7ff673117d4948ec16a85e85a9d14f27d988
m.tugarev <m.tugarev@1440.space>
2025-11-27 18:20:53 +0300

    fix rcon connection drop

--- internal/client/rcon.go
+++ internal/client/rcon.go
@@ -9,7 +9,8 @@ import (
 )
 
 type RCONClient struct {
-	conn *rcon.Conn
+	address  string
+	password string
 }
 
 // WaitForRCON wait 10 minutes for connect to RCON
@@ -26,26 +27,29 @@ func WaitForRCON(host string, password string) (client *RCONClient, err error) {
 }
 
 func NewRCONClient(address string, password string) (*RCONClient, error) {
-	conn, err := rcon.Dial(address, password)
-	if err != nil {
-		return nil, err
-	}
 	return &RCONClient{
-		conn: conn,
+		address:  address,
+		password: password,
 	}, nil
 }
 
-func (c *RCONClient) Close() error {
-	return c.conn.Close()
+func (c *RCONClient) execCommand(cmd string) (string, error) {
+	conn, err := rcon.Dial(c.address, c.password)
+	if err != nil {
+		return "", err
+	}
+	defer conn.Close()
+
+	return conn.Execute(cmd)
 }
 
 func (c *RCONClient) StopServer() error {
-	_, err := c.conn.Execute("stop")
+	_, err := c.execCommand("stop")
 	return err
 }
 
 func (c *RCONClient) ListPlayers() (int, error) {
-	resp, err := c.conn.Execute("list")
+	resp, err := c.execCommand("list")
 	if err != nil {
 		return 0, err
 	}
